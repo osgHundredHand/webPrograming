08fc4d446af6c4b91a534622a54540f4cd836ff6
OSG <notdonkatsu@gmail.com>
2021-01-03 01:36:49 +0900

    timer 함수 분리

--- Board/js/time.js
+++ Board/js/time.js
@@ -1,36 +1,60 @@
 $(document).ready(function(){
-    curruntTimeTo('#realTimeTimer');
+    timerCmd('#realTimeTimer');
+    // curruntTimeTo('#realTimeTimer');
 });
-
-function curruntTimeTo(loc){ 
+function timerCmd(loc){
     //====================================
-    // loc 로 targeting. (Realtime Timer, yyyy. mm. dd T hh:mm:ss)
+    // loc 로 targeting. (Realtime Timer, yyyy년 mm월 dd일 D요일 hh시 mm분 ss초)
     //====================================
-    
-    setInterval(function(){
-        var date = new Date(); //realtime 처리 위해 Date 객체 자체가 인터벌 생성되어야 함 -> method를 호출한다고 초기 Date값이 reflesh 되지 않음.
-        
-        var year = date.getFullYear();
-        var month = date.getMonth()+1; // 0~11    + 1
-        var dateFull = date.getDate(); // 1~31
-        var day = date.getDay(); // 0~6
-        var dayArrKor = ['일','월','화','수','목','금','토'];
+    init(loc);
+    timerInit();
+    clock(); 
+    timerExt();
+}
+function init(loc){
+    $loc = $(loc);
+}
+function timerInit(){
+    date = new Date();
+    year = date.getFullYear();
+    month = date.getMonth()+1; // 0~11    + 1
+    dateFull = date.getDate(); // 1~31
+    day = date.getDay(); // 0~6
+    dayArrKor = ['일','월','화','수','목','금','토'];
 
-        var datePrint = addZero(year) + '년' + addZero(month) + '월' + addZero(dateFull) + '일' + dayArrKor[day] + '요일';
-
-        var hour = date.getHours();
-        var minit = date.getMinutes();
-        var sec = date.getSeconds();
-
-        var timePrint = addZero(hour) + '시' + addZero(minit) + '분' + addZero(sec) +'초';
-
-        
-        $(loc).html(datePrint + timePrint);
-    },1000); 
+    hour = date.getHours();
+    minit = date.getMinutes();
+    sec = date.getSeconds();
+}
+function clock(){
+    //====================================
+    // new Date 객체를 생성 후 인스턴스값을 받아 시간을 출력하는 함수
+    //====================================
+    timerInit();
+    timerPrintTo( datePrint(year,month,dateFull,day), timePrint(hour,minit,sec) );
+}
+function timerExt(){
+    //====================================
+    // 시간 출력 함수를 0.5ms간격으로 지속 출력
+    //====================================
+    setInterval(clock,500);
+}
+function datePrint(year,month,date,day){ //hhhh년 mm월 dd일 d요일
+    var result = '';
+    result = addZero(year) + '년 ' + addZero(month) + '월 ' + addZero(date) + '일 ' + dayArrKor[day] + '요일 ';
+    return result;
+}
+function timePrint(hour,min,sec){
+    var result = '';
+    result = addZero(hour) + '시 ' + addZero(min) + '분 ' + addZero(sec) +'초 ';
+    return result;
 }
 function addZero(num){ // 1월~9월, 1일~9일, 01시 등 1자리 월일시분초 앞자리에 0을 추가하는 함수
     if(num < 10){
         num = '0' + num;
     }
     return num ;
+}
+function timerPrintTo(yearToDay,hourToSec){
+    $($loc).html(yearToDay + hourToSec);
 }
