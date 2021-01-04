71077b2d27ca76a47a8955c13a8f28b28c9cab7d
OSG <notdonkatsu@gmail.com>
2021-01-04 23:14:58 +0900

    banner.js 구조분리 및 test slide 기능 공사 중

--- Board/js/banner.js
+++ Board/js/banner.js
@@ -1,34 +1,43 @@
 $(document).ready(function(){
-    bannerSelectorCmd();
+    bannerSelectorCmd(6);
+ //   bannerSlider();
 });
 
-function bannerSelectorCmd(){
-    bannerSelectorInit();
+function bannerSelectorCmd(num){
+    //=========================
+    // 첫 li부터 num(max)까지 슬라이드 배너의 범위를 지정하면 동작하는 슬라이더 선택 함수.
+    //=========================
+    bannerSelectorInit(num);
     bannerSelectorExt();
-    resizeCal();
 }
-function bannerSelectorInit(){
+function bannerSelectorInit(num){
     numbefore = null;
     before = $('#main_ban_before'); // before a href id
     next = $('#main_ban_next')      // next a href id
+    maxSlidePageCount = parseInt(num);
 }
 function bannerSelectorExt(){
-    $(before).click(function(){
-        numbefore = currentBanNumb(before);
-        numbefore = parseInt(numbefore) - 1;
-        if(numbefore <= 1){ 
-            numbefore = 1;  
-        }
-         bannerSelector(numbefore);
-    });
-    $(next).click(function(){
-        numbefore = currentBanNumb(next);
-        numbefore = parseInt(numbefore) + 1;
-        if(numbefore >= 4){ // 배너 갯수 추가시 li 갯수에 맞춰 증설 => 수정 시 두 줄 값 전부 수정!!
-            numbefore = 4;  // 배너 갯수 추가시 li 갯수에 맞춰 증설 => 수정 시 두 줄 값 전부 수정!!
-        }
-        bannerSelector(numbefore);
-    });
+    $(before).click(swiftLeft);
+    $(next).click(swiftRight);
+    resizeCal();
+}
+function swiftLeft(){
+    numbefore = currentBanNumb(before);
+    numbefore = parseInt(numbefore) - 1;
+    
+    if(numbefore <= 1){ 
+        numbefore = 1;  
+    }
+    bannerSelector(numbefore);
+}
+function swiftRight(){
+    numbefore = currentBanNumb(next);
+    numbefore = parseInt(numbefore) + 1;
+    
+    if(numbefore >= maxSlidePageCount){ 
+        numbefore = maxSlidePageCount;  
+    }
+    bannerSelector(numbefore);
 }
 function currentBanNumb(beforeOrNext){
     return $(beforeOrNext).attr('href').substring(4,5);
@@ -38,9 +47,32 @@ function bannerSelector(numb){
     $(next).attr('href', '#ban' + numb);
 }
 function resizeCal(){
-    $(window).resize(function(){
-        var scrollXpos = $('.banner').width() * (numbefore - 1); // numbefore 초기값 null => 정수 0으로 취급
-        (scrollXpos < 0) ? scrollXpos = 0 : scrollXpos;        // numbefore 초기값 null => 정수 0으로 취급되어 page reload 초기 width가 음수로 나오는 부분을 0으로 set
-        $('.banner ol').scrollLeft(scrollXpos);
-    });
+    $(window).resize(bannerScroll);
 }
+function bannerScroll(){
+    var scrollXpos = $('.banner').width() * (numbefore - 1); // numbefore 초기값 null => 정수 0으로 취급
+    (scrollXpos < 0) ? scrollXpos = 0 : scrollXpos;        // numbefore 초기값 null => 정수 0으로 취급되어 page reload 초기 width가 음수로 나오는 부분을 0으로 set
+    $('.banner ol').scrollLeft(scrollXpos);
+}
+/*
+function bannerSlider(){
+    i = 0;
+    $banner = $('.banner');
+    bannerWidth = $banner.width();
+    maxScrollWidth = bannerWidth * (maxSlidePageCount - 1);
+    var interval = setInterval(slide,1);
+    if(i >= maxScrollWidth){
+        clearInterval(interval);
+    }
+}
+function slide(){
+    i=i+1;
+    $('.banner ol').scrollLeft(i);
+    
+}
+function reverseSlide(){
+    i=i-1;
+    $('.banner ol').scrollLeft(i);
+    
+}
+*/
