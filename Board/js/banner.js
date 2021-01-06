529a3993d8a36b119f8a26a2d4d03ba5e739c121
OSG <notdonkatsu@gmail.com>
2021-01-06 22:06:59 +0900

    배너 slider 개조

--- Board/js/banner.js
+++ Board/js/banner.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     bannerSelectorCmd(6);
- //   bannerSlider();
+    bannerSliderCmd();
 });
 
 function bannerSelectorCmd(num){
@@ -47,32 +47,67 @@ function bannerSelector(numb){
     $(next).attr('href', '#ban' + numb);
 }
 function resizeCal(){
-    $(window).resize(bannerScroll);
+ //   $(window).resize(bannerScroll); 현재 Slider와 같이 동작 안하므로 비활성화함..
 }
 function bannerScroll(){
     var scrollXpos = $('.banner').width() * (numbefore - 1); // numbefore 초기값 null => 정수 0으로 취급
     (scrollXpos < 0) ? scrollXpos = 0 : scrollXpos;        // numbefore 초기값 null => 정수 0으로 취급되어 page reload 초기 width가 음수로 나오는 부분을 0으로 set
     $('.banner ol').scrollLeft(scrollXpos);
 }
-/*
-function bannerSlider(){
+
+function bannerSliderCmd(){
+    bannerSliderInit();
+    bannerSlider();
+    mouseHoverStop();
+    mouseHoverRestart();
+}
+function bannerSliderInit(){
+    toggle = 1;
     i = 0;
+}
+function bannerSlider(){
     $banner = $('.banner');
-    bannerWidth = $banner.width();
-    maxScrollWidth = bannerWidth * (maxSlidePageCount - 1);
-    var interval = setInterval(slide,1);
-    if(i >= maxScrollWidth){
-        clearInterval(interval);
+    
+    if(toggle == 1){
+        interval = setInterval(slide,1);
+    }
+    if(toggle == -1){
+        interval = setInterval(reverseSlide,1);
     }
 }
+
 function slide(){
+    bannerWidth = $banner.width();
+    maxScrollWidth = bannerWidth * (maxSlidePageCount - 1);
     i=i+1;
+    if(i >= maxScrollWidth){
+        toggle = -1;
+        clearInterval(interval);
+        bannerSlider();
+    }
+    mouseHoverStop();
     $('.banner ol').scrollLeft(i);
     
 }
 function reverseSlide(){
+    var minScrollWidth = 0;
     i=i-1;
+    if(i <= minScrollWidth){
+        toggle = 1;
+        clearInterval(interval);
+        bannerSlider();
+    }
+    mouseHoverStop();
     $('.banner ol').scrollLeft(i);
-    
 }
-*/
+
+function mouseHoverStop(){
+    $banner.mouseenter(function(){
+        clearInterval(interval);
+    });
+}
+function mouseHoverRestart(){
+    $banner.mouseleave(function(){
+        bannerSlider();
+    });
+}
